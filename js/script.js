dd1a32b138c4011551422690134d0fdc6a875b63
beny81 <152813531+beny81@users.noreply.github.com>
2024-02-05 23:33:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,27 @@ document.addEventListener("DOMContentLoaded",
       function (event){ 
            
         
-              document.querySelector("#btt")
-                      .addEventListener("blur", function(event){
+              document.querySelector("#bell")
+                      .addEventListener("click", function(event){
                           
         
-                         
-          
+           
+(function(w, d) {
+  w.PushEngage = w.PushEngage || [];
+  w._peq = w._peq || [];
+  PushEngage.push(['init', {
+      appId: 'c4a2fd0e-b2bb-4edd-abb1-da49526d4b8a'
+  }]);
+
+  var e = d.createElement('script');
+
+  e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
+  e.async = true;
+  e.type = 'text/javascript';
+  d.head.appendChild(e);
+})(window, document);
+
                       
-                        let screenWidth = window.innerWidth;
-                         
-                     
-                                  if(screenWidth<769 ){
-                         
-                                   document.querySelector("#btt") .click();
-                                      
-                                  
-                                      
-                                     
-                                     
-                                      }
 
                           
                               
