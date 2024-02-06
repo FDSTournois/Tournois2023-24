9f5ebd338c3df28072d4780a7c119e19f226b423
beny81 <152813531+beny81@users.noreply.github.com>
2024-02-06 13:56:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,27 +12,38 @@ document.addEventListener("DOMContentLoaded",
                            
            
 (function(w, d) {
+    w.PushEngage = w.PushEngage || [];
+    w._peq = w._peq || [];
 
-    document.querySelector("#notifbutton2").click();
-  w.PushEngage = w.PushEngage || [];
-    // console.log( w.PushEngage);
-  w._peq = w._peq || [];
-  PushEngage.push(['init', {
-      appId: 'c4a2fd0e-b2bb-4edd-abb1-da49526d4b8a'
-  }]);
+    // Check if the user is already subscribed
+    if (Notification.permission === 'granted') {
+        // User is already subscribed, show a message or take some other action
+        console.log('You are already subscribed to notifications.');
+        // You can show a message to the user or perform any other action here
+    } else {
+        // User is not subscribed, initialize PushEngage and display notification prompt
+        PushEngage.push(['init', {
+            appId: 'c4a2fd0e-b2bb-4edd-abb1-da49526d4b8a'
+        }]);
 
-  var e = d.createElement('script');
-    
-     
-    
+        // Simulate click on notification button
+        document.querySelector("#notifbutton2").click();
+    }
 
-  e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
-  e.async = true;
-  e.type = 'text/javascript';
-  d.head.appendChild(e);
-      console.log(e);
+    var e = d.createElement('script');
+    if (e) {
+        e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
+        e.async = true;
+        e.type = 'text/javascript';
+
+        // Append PushEngage SDK script to the head of the document
+        d.head.appendChild(e);
+    }
+
+    // Simulate click on close button (if needed)
     document.querySelector("#closebutton").click();
 })(window, document);
+
 
                       
 
