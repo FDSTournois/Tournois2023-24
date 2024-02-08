fceff1c6d972c12186f0df34ada457e81e8712a5
beny81 <152813531+beny81@users.noreply.github.com>
2024-02-08 10:24:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
 
-(function(w, d) {
-  w.PushEngage = w.PushEngage || [];
-  w._peq = w._peq || [];
-  PushEngage.push(['init', {
-      appId: 'c4a2fd0e-b2bb-4edd-abb1-da49526d4b8a'
-  }]);
-
-  var e = d.createElement('script');
-  console.log("hello Big G");
-
-  e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
-  e.async = true;
-  e.type = 'text/javascript';
-  d.head.appendChild(e);
-})(window, document);
